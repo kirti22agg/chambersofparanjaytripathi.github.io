9d789d87278ae982391e6a6fc54ac0dac940c3fd
kirti22agg <kirti22agg@gmail.com>
2023-08-13 14:55:46 +0530

    chambers of paranjay tripathi

---
+++ final.js
@@ -0,0 +1,35 @@
+window.addEventListener("load",function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display="block";
+        },
+        1000
+    )
+});
+document.querySelector("#close").addEventListener("click",function(){
+    document.querySelector(".popup").style.display="none";
+});
+let menuIcon = document.querySelector('.menuIcon');
+        let nav = document.querySelector('.overlay-menu');
+
+        menuIcon.addEventListener('click', () => {
+            if (nav.style.transform != 'translateX(0%)') {
+                nav.style.transform = 'translateX(0%)';
+                nav.style.transition = 'transform 0.2s ease-out';
+            } else { 
+                nav.style.transform = 'translateX(-100%)';
+                nav.style.transition = 'transform 0.2s ease-out';
+            }
+        });
+
+
+        // Toggle Menu Icon ========================================
+        let toggleIcon = document.querySelector('.menuIcon');
+
+        toggleIcon.addEventListener('click', () => {
+            if (toggleIcon.className != 'menuIcon toggle') {
+                toggleIcon.className += ' toggle';
+            } else {
+                toggleIcon.className = 'menuIcon';
+            }
+        });
